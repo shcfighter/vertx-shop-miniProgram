547e2da681dd6441ed8e485784140798e5566376
git <825496059@qq.com>
2018-11-08 11:31:02 +0800

    coupon

--- src/main/java/com/ecit/shop/constants/CouponSql.java
+++ src/main/java/com/ecit/shop/constants/CouponSql.java
@@ -8,6 +8,6 @@ public interface CouponSql {
     /**
      * 获取代金券列表
      */
-    String SELECT_COUPON_SQL = "select coupon_name, coupon_type, coupon_amount, min_user_amount, expiry_date from t_coupon where begin_time >= ? and end_time <= ? and is_deleted = 0 order by sort";
+    String SELECT_COUPON_SQL = "select coupon_name, coupon_type, coupon_amount, min_user_amount, expiry_date from t_coupon where begin_time <= ? and end_time >= ? and is_deleted = 0 order by sort";
 
 }
--- src/main/java/com/ecit/shop/handler/impl/CouponHandler.java
+++ src/main/java/com/ecit/shop/handler/impl/CouponHandler.java
@@ -7,9 +7,11 @@ import com.ecit.shop.handler.ICouponHandler;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.Vertx;
 
+import java.util.Date;
 import java.util.List;
 
 public class CouponHandler extends JdbcRxRepositoryWrapper implements ICouponHandler {
@@ -21,7 +23,7 @@ public class CouponHandler extends JdbcRxRepositoryWrapper implements ICouponHan
     @Override
     public ICouponHandler findCouponList(Handler<AsyncResult<List<JsonObject>>> handler) {
         Future<List<JsonObject>> addressFuture = Future.future();
-        this.retrieveAll(CouponSql.SELECT_COUPON_SQL).subscribe(addressFuture::complete, addressFuture::fail);
+        this.retrieveMany(new JsonArray().add(new Date()).add(new Date()), CouponSql.SELECT_COUPON_SQL).subscribe(addressFuture::complete, addressFuture::fail);
         addressFuture.setHandler(handler);
         return this;
     }
