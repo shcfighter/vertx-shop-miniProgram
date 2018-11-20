4a35e034b385506371f88c8d56ea70812affc600
git <825496059@qq.com>
2018-11-20 12:14:58 +0800

    order

--- src/main/java/com/ecit/shop/constants/OrderSql.java
+++ src/main/java/com/ecit/shop/constants/OrderSql.java
@@ -8,7 +8,7 @@ public interface OrderSql {
     /**
      * 下单
      */
-    String INSERT_ORDER_SQL = "insert into t_order(order_id, user_id, province_id, city_id, district_id, province_value, city_value, district_value, receiver, address, mobile, postcode, create_time, order_details, total_price, order_status, coupon_id, coupon_price, freight_price, actual_price, remarks) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, now(), ?, ?, ?, ?, ?, ?, ?, ?)";
+    String INSERT_ORDER_SQL = "insert into t_order(order_id, user_id, province_id, city_id, district_id, province_value, city_value, district_value, receiver, address, mobile, postcode, create_time, order_details, total_price, order_status, coupon_id, coupon_price, freight_price, actual_price, remarks) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, now(), ?, ?, ?, ?, ?, ?, ?, ?)";
 
     /**
      * 分页查询订单列表
